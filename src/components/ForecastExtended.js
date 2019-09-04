1badd455525ebee1e3ed750b1c2f3c9004649e72
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-09-04 16:19:25 -0300

    finish reacts components and coming redux aplication

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,17 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForescastItem';
+import { api_key, url_base_forecast } from './../constants/api_url';
+import transformForecast from './../services/transformForecast';
 import './styles.css';
-
-const days = [
-    'Lunes',
-    'Martes',
-    'Miercoles',
-    'Jueves',
-    'Viernes',
-    'Sabado',
-    'Domingo',
-];
 
 class ForecastExtended extends Component {
 
@@ -20,26 +12,58 @@ class ForecastExtended extends Component {
         this.state = { forecastData: null, }    
     }
 
-    renderForecastItemDays() {
-        return (
-            days.map( day => (
-                <ForecastItem weekDay={day} hour={10}></ForecastItem>
-                ))
-                );
+    componentDidMount() {
+        this.updateCity(this.props.city);
+    }
+
+    componentDidUpdate = (prevProps) => {
+        if(prevProps.city !== this.props.city){
+            console.log("prop de ciudad previa",prevProps.city)
+            console.log("prop de ciudad actual actualizada en el render",this.props.city)
+            this.setState({forecastData:null})
+            this.updateCity(this.props.city);//this.props.ciudad es la que vienen al hacer click y se actualiza cuando entra al render
+        }
+    }
+    
+    updateCity = city => {
+        const url_forecast = `${url_base_forecast}?q=${city}&APPID=${api_key}&units=metric`;
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                const forecastData = transformForecast(weather_data);
+                this.setState({ forecastData });
             }
+        );
+    }
+    renderForecastItemDays(forecastData) {
+        return (
+            forecastData.map( forecast => (
+                <ForecastItem 
+                    key={`${forecast.weekDay}${forecast.hour}`}
+                    weekDay={forecast.weekDay} 
+                    hour={forecast.hour}
+                    data={forecast.data}>
+                </ForecastItem>
+            ))
+        );
+    }
+    renderProgress = () => {
+        return <h3>cargando pronostico extendido</h3>;
+    }
             
     render() {
         const { city } = this.props;
         const { forecastData } = this.state;
         return (
             <div>
-                <h2 className='forecast-title '>                    
+                <h2 className='forecast-title'>                    
                     Pronostico extendido para { city }
                 </h2>
                 { 
                     forecastData ? 
-                        this.renderForecastItemDays() :
-                        this.renderPogress()
+                        this.renderForecastItemDays(forecastData) :
+                        this.renderProgress()
                 }
             </div>
         );
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,3 +1,4 @@
 export const api_key = '59ee020710f0bcfb07b99fe2f1ed34ab';
 export const url_base_weather = 'https://api.openweathermap.org/data/2.5/weather';
+export const url_base_forecast = 'https://api.openweathermap.org/data/2.5/forecast';
 
